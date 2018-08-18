f2be39e5e9d08e15a8766c7f0bb155a2693d037c
bre7 <bre7@users.noreply.github.com>
2018-08-18 19:15:21 -0300

    Added SVG to test

--- test/basic.js
+++ test/basic.js
@@ -1,9 +1,12 @@
+const assert = require('assert');
 const { setupBundler } = require('./utils');
 const assertBundleTree = require('parcel-assert-bundle-tree');
 const path = require('path');
+const fs = require('fs')
 
 describe('basic', function() {
   it('Should create a basic imagemin bundle', async function() {
+    this.timeout(0);
     const bundler = await setupBundler(path.join(__dirname, './Integration/Basic/index.html'), {
       production: true
     });
@@ -21,8 +24,13 @@ describe('basic', function() {
         },
         {
           type: 'jpeg'
+        },
+        {
+          type: 'svg'
         }
       ]
     });
+
+    // TODO: Test params
   });
 });
