a614f4643b0efad1421ae4723776b5507fca55a3
bre7 <bre7@users.noreply.github.com>
2018-08-18 19:17:04 -0300

    Added support for extra parameters for each minifier (same API)

---
+++ test/Integration/Basic/imagemin.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
+  "mozjpeg": { "progressive": true, "quality": 10 },
+  "pngquant": { "quality": 10 },
+  "svgo": {
+    "plugins": [
+      { "removeViewBox": false },
+      { "cleanupIDs": true },
+    ]
+  }
+}
