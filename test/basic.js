78717f69ce333d6aec674b34fd614958cb2bc92f
Jasper De Moor <jasperdemoor@gmail.com>
2019-02-11 21:02:49 +0100

    prettify tests

--- test/basic.js
+++ test/basic.js
@@ -46,7 +46,10 @@ describe('basic', function() {
     });
 
     let originalSvgContent = await fs.readFile(path.join(__dirname, './Integration/Basic/image4.svg'), 'utf8');
-    let svgOutput = await fs.readFile(Array.from(bundle.childBundles.values()).find(value => value.type === 'svg').name, 'utf8');
+    let svgOutput = await fs.readFile(
+      Array.from(bundle.childBundles.values()).find(value => value.type === 'svg').name,
+      'utf8'
+    );
     assert(originalSvgContent.includes('id="SVGID_1_"'));
     assert(originalSvgContent.includes('viewBox'));
     assert(!svgOutput.includes('id="SVGID_1_"'));
@@ -54,23 +57,33 @@ describe('basic', function() {
 
     let originalPngImage = await fs.readFile(path.join(__dirname, './Integration/Basic/image2.png'));
     let jsBundle = Array.from(bundle.childBundles.values()).find(value => value.type === 'js');
-    let minifiedPngImage = await fs.readFile(Array.from(jsBundle.childBundles.values()).find(value => value.type === 'png').name);
+    let minifiedPngImage = await fs.readFile(
+      Array.from(jsBundle.childBundles.values()).find(value => value.type === 'png').name
+    );
     assert(originalPngImage.byteLength > minifiedPngImage.byteLength);
 
     let originalJpegImage = await fs.readFile(path.join(__dirname, './Integration/Basic/image3.jpeg'));
-    let minifiedJpegImage = await fs.readFile(Array.from(bundle.childBundles.values()).find(value => value.type === 'jpeg').name);
+    let minifiedJpegImage = await fs.readFile(
+      Array.from(bundle.childBundles.values()).find(value => value.type === 'jpeg').name
+    );
     assert(originalJpegImage.byteLength > minifiedJpegImage.byteLength);
 
     let originalGifImage = await fs.readFile(path.join(__dirname, './Integration/Basic/image1.gif'));
-    let minifiedGifImage = await fs.readFile(Array.from(bundle.childBundles.values()).find(value => value.type === 'gif').name);
+    let minifiedGifImage = await fs.readFile(
+      Array.from(bundle.childBundles.values()).find(value => value.type === 'gif').name
+    );
     assert(originalGifImage.byteLength > minifiedGifImage.byteLength);
 
     let originalJpgImage = await fs.readFile(path.join(__dirname, './Integration/Basic/images/image5.jpg'));
-    let minifiedJpgImage = await fs.readFile(Array.from(bundle.childBundles.values()).find(value => value.type === 'jpg').name);
+    let minifiedJpgImage = await fs.readFile(
+      Array.from(bundle.childBundles.values()).find(value => value.type === 'jpg').name
+    );
     assert(originalJpgImage.byteLength > minifiedJpgImage.byteLength);
 
     let originalWebpImage = await fs.readFile(path.join(__dirname, './Integration/Basic/image6.webp'));
-    let minifiedWebpImage = await fs.readFile(Array.from(bundle.childBundles.values()).find(value => value.type === 'webp').name);
+    let minifiedWebpImage = await fs.readFile(
+      Array.from(bundle.childBundles.values()).find(value => value.type === 'webp').name
+    );
     assert(originalWebpImage.byteLength > minifiedWebpImage.byteLength);
   });
 
